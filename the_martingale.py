0bdce79fe42f36e8f3e19c1a75d10f5a896ae644
Felipe J Franca Jordao <felipejohnata@msn.com>
2019-09-14 08:15:03 -0400

    Update the_martingale.py

--- the_martingale.py
+++ the_martingale.py
@@ -25,10 +25,11 @@ def Simulate(bankroll, spinLimit):
 
 def Play():
     spin = randint(0,37)
+    pockets = [15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26]
     result = -1
     if spin == 0 or spin == 37:
         return 0
-    elif spin % 2 == 0:
+    elif spin in(pockets):
         return 1
         
     return 0
@@ -41,16 +42,22 @@ def main():
     loss = 0
     win = 0
     data = []
+    acum_win = 0
+    acum_loss = 0
     for i in range(trials):
         sim = Simulate(bankroll, spins)
         data.append(sim - bankroll)
         if sim > bankroll:
             win += 1
-            print("Net: $" + str(sim - bankroll))
+            acum_win += sim - bankroll
         else:
             loss += 1
-            print("LOST. Remaining Bal: $" + str(sim))
-            
+            acum_loss += bankroll - sim
+
+    print("Winnings: $" + str(acum_win))
+    print("Losses: $" + str(acum_loss))
+    print("% of Earnings: " + str((acum_win/(acum_win + acum_loss))*100 ) + " %")
+    print("------------------------------------------")
     print("Losses: " + str(loss))
     print("Winnings: " + str(win))
     print("Stats: " + str(( win / (i + 1))*100) + " %")
@@ -60,11 +67,9 @@ def main():
     plt.title('Histogram - Problem 4')
     plt.xlabel('Data value')
     plt.ylabel('Frequency')
-    plt.savefig('The_Martingale.pdf', bbox_inches='tight')
-    plt.show()
-    
+    plt.savefig('The_Martingale.png', bbox_inches='tight')
+
     
 
-if __name__ == '__main__':
-    main()   
+main()   
     
